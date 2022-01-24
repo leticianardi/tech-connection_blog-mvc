92b74b55fcbc97236ea5431c92762cf84a7be6fe
leticianardi <leticia.tnardi@gmail.com>
2022-01-23 19:28:20 -0800

    home and index routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/logout", (req, res) => {
+  res.render("logout");
+});
+
+router.get("/homepage", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_title", "post_content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((posts) => {
+      const posts = posts.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_title", "post_content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((posts) => {
+      if (!posts) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = posts.get({ plain: true });
+      post.post_content = post.post_content.split("\n");
+
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
